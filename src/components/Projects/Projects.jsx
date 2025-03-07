27c5ac966f131f4f507fe36fbafdd0d90cc844a1
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-03-07 08:38:05 +0530

    change in Projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Projects.css";
-import { FaGithub, FaReact, FaNodeJs } from "react-icons/fa";
+import { FaGithub, FaReact, FaNodeJs, FaBootstrap } from "react-icons/fa";
 import {
   SiMongodb,
   SiRedux,
   SiKicad,
   SiSocketdotio,
   SiBlender,
+  SiAngular,
+  SiDotnet,
+  SiSass,
 } from "react-icons/si";
 
 const Projects = () => {
@@ -16,6 +19,34 @@ const Projects = () => {
       <div class="project-containers">
         <div class="top-row">
           <div class="div1">
+            <div className="project-title">
+              <b>
+                <i>Zaptern - Intern Opportunity Management Portal</i>
+              </b>
+              <span class="github-icon">
+                <a
+                  href="https://github.com/vishwaDeshan/Zaptern-SOMP-Frontend"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub style={{ color: "white" }} />
+                </a>
+              </span>
+            </div>
+            <div className="project-description">
+              An internship management system designed to streamline the process
+              of managing internships. It uses clean architecture with the CQRS
+              pattern, along with ASP.NET for the backend and NgRx for state
+              management in Angular.
+            </div>
+            <div className="tech">
+              <SiAngular size={30} />
+              <FaBootstrap size={30} />
+              <SiDotnet size={30} />
+              <SiSass size={30} />
+            </div>
+          </div>
+          <div class="div2">
             <div className="project-title">
               <b>
                 <i>EduMor </i> ~ Learning Management System{" "}
@@ -37,10 +68,44 @@ const Projects = () => {
               Flutter. I worked as the project's group leader.
             </div>
             <div className="tech">
-              <FaReact size={30}/>
-              <FaNodeJs size={30}/>
-              <SiMongodb size={30}/>
-              <SiRedux size={30}/>
+              <FaReact size={30} />
+              <FaNodeJs size={30} />
+              <SiMongodb size={30} />
+              <SiRedux size={30} />
+            </div>
+          </div>
+        </div>
+        <div class="top-row">
+          <div class="div1">
+            <div className="project-title">
+              <b>Real Time Sharing White Board</b>
+              <span class="github-icon" url>
+                <a
+                  href="https://github.com/vishwaDeshan/Real-Time-Whiteboard"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub style={{ color: "white" }} />
+                </a>
+              </span>
+            </div>
+            <div
+              className="project-description div3-desc"
+              style={{ fontSize: "20px" }}
+            >
+              This project is aimed at building a real-time sharing whiteboard.
+              It is developed using the MERN stack along with Socket.IO, which
+              enables real-time communication between multiple users. The
+              whiteboard allows multiple users to collaborate on a shared
+              canvas, in real-time. Users can draw and add different types of
+              media to the canvas, which is then immediately visible to all
+              other participants.
+            </div>
+            <div className="tech">
+              <FaReact size={30} />
+              <FaNodeJs size={30} />
+              <SiMongodb size={30} />
+              <SiSocketdotio size={30} />
             </div>
           </div>
           <div class="div2">
@@ -65,41 +130,25 @@ const Projects = () => {
               this project.
             </div>
             <div className="tech">
-              <SiKicad size={30}/>
-              <SiBlender size={30}/>
+              <SiKicad size={30} />
+              <SiBlender size={30} />
             </div>
           </div>
         </div>
-        <div class="bottom-row">
-          <div class="div3">
-            <div className="project-title">
-              <b>Real Time Sharing White Board</b>
-              <span class="github-icon" url>
-                <a
-                  href="https://github.com/vishwaDeshan/Real-Time-Whiteboard"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaGithub style={{ color: "white" }} />
-                </a>
-              </span>
+      </div>
+      <div className="explore-more-button">
+        <button type="button">
+          <a
+            href="https://github.com/vishwaDeshan?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Explore more projects
+            <div className="icon">
+              <FaGithub style={{ color: "white" }} size={30} />
             </div>
-            <div className="project-description div3-desc" style={{ fontSize: "20px" }}>
-              This project is aimed at building a real-time sharing whiteboard.
-              It is developed using the MERN stack along with Socket.IO, which enables real-time
-              communication between multiple users. The whiteboard allows
-              multiple users to collaborate on a shared canvas, in real-time.
-              Users can draw and add different types of media to the canvas,
-              which is then immediately visible to all other participants.
-            </div>
-            <div className="tech">
-              <FaReact size={30}/>
-              <FaNodeJs size={30}/>
-              <SiMongodb size={30}/>
-              <SiSocketdotio size={30}/>
-            </div>
-          </div>
-        </div>
+          </a>
+        </button>
       </div>
     </div>
   );
